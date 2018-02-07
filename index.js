151523e9f9e2dcbb72ef899e97b885e63ce92fbb
Mila J <icecold240@gmail.com>
2018-02-07 20:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = [];
-  for (let i = 0, musicians.length = 1; i < 1; i++) {
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
     arr.push('${musicians[i]} plays ${instruments[i]}')
   }
 }
