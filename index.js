a99e8f84b9d4cf84edd84859e85e9cbd3030ec92
Mila J <icecold240@gmail.com>
2018-02-07 20:24:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
+// function theBeatlesPlay(musicians, instruments) {
+//   const arr = [];
+//   for (let i = 0, 1 = musicians.length; i < 1; i++) {
+//     arr.push('${musicians[i]} plays ${instruments[i]}')
+//   }
+// }
+//
+// function johnLennonFacts(facts) {
+//   const yelling = [];
+//   let i = 0;
+//
+//   while (i < facts.length) {
+//     yelling.push('${facts[i]}!!!)';
+//     i++;
+//   }
+//   return yelling;
+// }
+//
+// function iLoveTheBeatles(number) {
+//   const love = [];
+//   do {
+//     love.push('I love the Beatles!');
+//     i++;
+//   } while (n < 15)
+//   return love;
+// }
 function theBeatlesPlay(musicians, instruments) {
-  const arr = [];
-  for (let i = 0, 1 = musicians.length; i < 1; i++) {
-    arr.push('${musicians[i]} plays ${instruments[i]}')
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+
+  return arr
 }
 
 function johnLennonFacts(facts) {
-  const yelling = [];
-  let i = 0;
+  const shoutedFacts = []
 
+  let i = 0
   while (i < facts.length) {
-    yelling.push('${facts[i]}!!!)';
-    i++;
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
   }
-  return yelling;
+
+  return shoutedFacts
 }
 
-function iLoveTheBeatles(number) {
-  const love = [];
+function iLoveTheBeatles(n) {
+  const arr = []
+
   do {
-    love.push('I love the Beatles!');
-    i++;
+    arr.push('I love the Beatles!')
+    n++
   } while (n < 15)
-  return love;
+
+  return arr
 }
