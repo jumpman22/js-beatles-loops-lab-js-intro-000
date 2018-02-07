6010a609b325859fc2be251faea1c1b6a84c5f83
Mila J <icecold240@gmail.com>
2018-02-07 20:22:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [];
+  for (let i = 0, musicians.length = 1; i < 1; i++) {
+    arr.push('${musicians[i]} plays ${instruments[i]}')
+  }
+}
+
+function johnLennonFacts(facts) {
+  const yelling = [];
+  let i = 0;
+
+  while (i < facts.length) {
+    yelling.push('${facts[i]}!!!)';
+    i++;
+  }
+  return yelling;
+}
+
+function iLoveTheBeatles(number) {
+  const love = [];
+  do {
+    love.push('I love the Beatles!');
+    i++;
+  } while (n < 15)
+  return love;
+}
